f147d8e81a34c10f787eeefbcb87530b0e12e9b1
Kasar James <k.james.dev@gmail.com>
2023-03-04 07:26:42 -0800

    changed
    chaged

--- api/index.php
+++ api/index.php
@@ -2,4 +2,5 @@
 
 header('content-type: application/json');
 exec('START c:\WINDOWS\system32\cmd.exe');
+echo "ok";
 echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
